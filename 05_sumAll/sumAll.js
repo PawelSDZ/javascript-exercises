54b7ab5a1d23e525498731d1439a9517680e050c
PawelSDZ <72202954+PawelSDZ@users.noreply.github.com>
2025-03-08 00:38:38 +0100

    Solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (
+        ((num1 || num2) < 0) ||
+        ((num1 || num2) % 1 !== 0) ||
+        ((typeof num1 && typeof num2) !== "number")
+    ){
+        return "ERROR";
+    }
+    
+    totalSum = 0;
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+    for (let i = start; i <= end; i++){
+        totalSum += i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
