0522caabeb314839c55e8e151ada519d5521d934
PawelSDZ <72202954+PawelSDZ@users.noreply.github.com>
2025-03-07 21:33:37 +0100

    Solved fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...moreRemovals) {
+    for (argument of moreRemovals) {
+        if (array.includes(argument)) {
+            removalIndex = array.indexOf(argument);
+            array.splice(removalIndex,1);
+            if (array.indexOf(argument) != -1 ) {
+                removalIndex = array.indexOf(argument);
+                array.splice(removalIndex,1);
+        }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
