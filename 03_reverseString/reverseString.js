5853780f329f237e924f89dc84aca949f368531b
PawelSDZ <72202954+PawelSDZ@users.noreply.github.com>
2025-03-07 04:21:46 +0100

    Solved third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    const words = string.split(" ");
+    reversedWords = [];
+    for (let word of words) {
+        reversedWords.push(word.split("").reverse().join(""));
+    }
+    reversedWords.reverse();
+    return reversedWords.join(" ")
+}
+   
 
 // Do not edit below this line
 module.exports = reverseString;
