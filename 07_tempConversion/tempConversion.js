e1b0ec52bf32ce367d3f956ad226b2853828c3f1
PawelSDZ <72202954+PawelSDZ@users.noreply.github.com>
2025-03-08 01:55:40 +0100

    Solved the last exercise of the assigment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) { 
+  const celsiusTemperature = Math.round(((temperature - 32) * 5/9) * 10)
+   / 10;
+  return celsiusTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheitTemperature = Math.round(((temperature * 9/5) + 32) *10)
+  / 10;
+  return fahrenheitTemperature;
 };
 
 // Do not edit below this line
